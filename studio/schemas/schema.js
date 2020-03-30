03f2053439e3cb4ac1bf598eda9c16ee83c6b82b
NRD89 <ndaltondev@gmail.com>
2020-03-30 14:04:06 -0700

    first commit

---
+++ studio/schemas/schema.js
@@ -0,0 +1,26 @@
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import stateCannabisInfo from "./stateCannabisInfo";
+import post from "./post";
+import product from "./product";
+import ctaSection from "./ctaSection";
+import quoteSection from "./quoteSection";
+import source from "./source";
+import figure from "./figure";
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: "default",
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    stateCannabisInfo,
+    post,
+    product,
+    ctaSection,
+    quoteSection,
+    source,
+    figure
+  ])
+});
