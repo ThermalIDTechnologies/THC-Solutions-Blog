8201a54da4a77a3f0151f90fc403c4f3d5353a92
NRD89 <ndaltondev@gmail.com>
2020-04-06 19:25:31 -0700

    added: fixed width to button component for consistency

--- web/src/components/styles/StyledButton.js
+++ web/src/components/styles/StyledButton.js
@@ -4,13 +4,15 @@ export const Btn = styled.a(
   props => css`
     background-color: ${props.bgColor};
     color: ${props.fontColor};
-    padding: 0.5rem 2rem;
+    padding: 0.5rem 0;
     text-decoration: none;
+    text-align: center;
     border-radius: 0.9rem;
     -webkit-box-shadow: 3px 3px 7px 0px rgba(0, 0, 0, 0.6);
     -moz-box-shadow: 3px 3px 7px 0px rgba(0, 0, 0, 0.6);
     box-shadow: 3px 3px 7px 0px rgba(0, 0, 0, 0.6);
     font-weight: 500;
+    width: 12.5rem;
     ${'' /* margin: 0 auto; */}
   `
 )
