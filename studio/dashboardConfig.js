d5ebc6a28d7231212d48b63599be8836ecddcf68
NRD89 <ndaltondev@gmail.com>
2020-06-17 11:01:14 -0700

    added: Netlify build info for netlify dashboard plugin. updated: Sanity Studio title.

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -26,6 +26,12 @@ export default {
             buildHookId: '5ee7b33f1d0fd56810dd2038',
             name: 'thc-link-in-bio',
           },
+          {
+            title: 'regulations.thclabelsolutions.com',
+            apiId: '6059a409-8639-41ef-8ddf-9834b745b0ee',
+            buildHookId: '5eea59bdeaeda901f9a8b4ec',
+            name: 'thc-us-state-policy-site',
+          },
         ]
       }
     },
